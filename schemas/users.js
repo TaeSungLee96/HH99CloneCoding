be6de698b70b49ef1900be243673cea56200be1c
Lee MiHwa <bbs2824@naver.com>
2022-04-16 13:30:44 +0900

    작업 전 기본코드 세팅

---
+++ schemas/users.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const usersSchema = mongoose.Schema({
+
+  // Users
+
+  userId: {
+    type: String,
+    required: true,
+  },
+  userPassword: {
+    type: String,
+    required: true,
+  },
+  userNickname: {
+    type: String,
+    required: true,
+  },
+  userGu: {
+    type: String,
+    required: true,
+  },
+   userDong: {
+    type: String,
+    required: true,
+  },
+  //userImage default = 임의image
+  userImage: {
+    type: String,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model("users", usersSchema);
