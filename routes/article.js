f51f663373df61251a29f7f219276c1e112c4840
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-04-19 17:09:06 +0900

    s.8

--- routes/article.js
+++ routes/article.js
@@ -247,7 +247,8 @@ router.get("/detail/:articleNumber", authMiddleware, async (req, res) => {
         //articleNumber가 일치하는 것
         const list = await Articles.find({ articleNumber });
         //List.userId가 같은 것만 가져옴
-        const userImage = (await Users.findOne({ userId: list.userId })).userImage;
+        const users = await Users.findOne({ userId: list.userId });
+        const userImage = users.userImage
         //좋아요 갯수
         const totalLike = (await Likes.find({ articleNumber })).length;
         const List = {list,userImage,totalLike}
