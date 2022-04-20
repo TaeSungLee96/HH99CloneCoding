dd5d9e4de0db28424f227b5ad92c19df0ef21ceb
qwe1234k <bbyl0124@gmail.com>
2022-04-20 17:11:31 +0900

    중간 커밋

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ connect();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
-//const port = 3000;
+const port = 3000;
 const mainRouter = require("./routes/main");
 const userRouter = require("./routes/user");
 const articleRouter = require("./routes/article");
-const chatRouter = require("./routes/chat");
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-// http server를 socket.io server로 upgrade한다
 
+// http server를 socket.io server로 upgrade한다
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+// const chatRouter = require("./routes/chat");
 
 // 접속 로그 남기기
 const requestMiddleware = (req, res, next) => {
@@ -32,7 +33,6 @@ const requestMiddleware = (req, res, next) => {
 // 각종 미들웨어
 app.use(cors({ credentials: true }));
 app.use(express.json());
-app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(requestMiddleware);
 app.use(express.urlencoded({ extended: false }));
@@ -42,35 +42,29 @@ app.use(express.static("uploads"));
 app.use("/main", [mainRouter]);
 app.use("/user", [userRouter]);
 app.use("/article", [articleRouter]);
-app.use("/chat", [chatRouter]);
+// app.use("/chat", [chatRouter]);
 
-// // 서버 열기
-// app.listen(port, () => {
-//   console.log(port, "포트로 서버가 켜졌어요!");
-// });
-
-server.listen(3000, function() {
-  console.log('Socket IO server listening on port 3000');
+// chat 경로로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index-room.html");
 });
 
-// // chat 경로로 서버에 접속하면 클라이언트로 index.html을 전송한다
-// router.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/../index-room.html');
-// });
+// namespace /chat에 접속한다.
+var chat = io.of("/chat").on("connection", function (socket) {
+  socket.on("chat message", function (data) {
+    console.log("message from client: ", data);
 
-// // namespace /chat에 접속한다.
-// var chat = io.of('/room').on('connection', function(socket) {
-//   socket.on('chat message', function(data){
-//     console.log('message from client: ', data);
+    var name = (socket.name = data.name);
+    var room = (socket.room = data.room);
 
-//     var name = socket.name = data.name;
-//     var room = socket.room = data.room;
+    // room에 join한다
+    socket.join(room);
+    // room에 join되어 있는 클라이언트에게 메시지를 전송한다
+    chat.to(room).emit("chat message", data.msg);
+  });
+});
 
-//     // room에 join한다
-//     socket.join(room);
-//     // room에 join되어 있는 클라이언트에게 메시지를 전송한다
-//     chat.to(room).emit('chat message', data.msg);
-//   });
-// });
-
-
+// // 서버 열기
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 켜졌어요!");
+});
