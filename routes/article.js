b5f0a73d2ef64d4fab4058bc9485172d130ff784
qwe1234k <bbyl0124@gmail.com>
2022-04-21 16:01:54 +0900

    좋아요 조건문 분기 이상이상

--- routes/article.js
+++ routes/article.js
@@ -416,7 +416,7 @@ router.post("/like", authMiddleware, async (req, res) => {
       //사용유자가 같은 상품에 좋아요를 했는지 확인
       const like = await Likes.find({ articleNumber, userId: user.userId });
       console.log("[like]", like);
-      if (like) {
+      if (like.length) {
         //일치하는 갚은 있으면 삭제
         await Likes.deleteOne({ articleNumber, userId: user.userId });
         //남은 개수
