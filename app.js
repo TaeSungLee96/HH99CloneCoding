3ab8a9e5d5950f4666ff46c210fb4bd164b0dea5
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-04-21 09:53:08 +0900

    s.26

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+// const io = require("socket.io")(server);
+const connect = require("./schemas/index");
+connect();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const port = 3000;
+const mainRouter = require("./routes/main");
+const userRouter = require("./routes/user");
+const articleRouter = require("./routes/article");
+
+// 접속 로그 남기기
+const requestMiddleware = (req, res, next) => {
+  console.log(
+    "[Ip address]:",
+    req.ip,
+    "[method]:",
+    req.method,
+    "Request URL:",
+    req.originalUrl,
+    " - ",
+    new Date()
+  );
+  next();
+};
+
+// 각종 미들웨어
+app.use(cors({ credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(requestMiddleware);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("uploads"));
+
+//라우터 연결
+app.use("/main", [mainRouter]);
+app.use("/user", [userRouter]);
+app.use("/article", [articleRouter]);
+
+// 서버 열기
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 켜졌어요!");
+});
